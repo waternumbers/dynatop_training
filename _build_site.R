2cd730eec472c1b1da3e284b8328e0812b1bb5d1
Paul Smith <paul@waternumbers.co.uk>
2021-09-22 21:35:29 +0100

    Altered after comments

---
+++ _build_site.R
@@ -0,0 +1,26 @@
+## commands to build the training site
+rm(list=ls())
+
+## create the zip file of data
+unlink("eden_data.zip")
+setwd("./_create_data")
+zip("../eden_data.zip","./eden_data")
+setwd("../")
+
+## create the zip file of R scripts
+tmp <- list.files(".",pattern=".Rmd$")
+out <- sapply(tmp,function(x){knitr::purl(x,documentation = 0,quiet=TRUE)})
+unlink("dynatop_training_rcode.zip")
+zip("dynatop_training_rcode.zip",out[file.size(out)>1])
+unlink(out)
+
+## remove existing folder of results and unzip zip file
+unlink("./eden_data",recursive=TRUE)
+unzip("eden_data.zip")       
+
+## run to build the site
+rmarkdown::clean_site(quiet=TRUE)
+rmarkdown::render_site() #quiet=TRUE)
+
+## create
+
