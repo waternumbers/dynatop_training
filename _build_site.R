3ad2c5a3cc905cd009570178bd13e50bccbb6268
Paul Smith <paul@waternumbers.co.uk>
2024-05-06 17:03:05 +0100

    further work

--- _build_site.R
+++ _build_site.R
@@ -1,11 +1,12 @@
 ## commands to build the training site
 rm(list=ls())
 
-## create the zip file of data
-unlink("eden_data.zip")
-setwd("./_create_data")
-zip("../eden_data.zip","./eden_data")
-setwd("../")
+## Not sure where all of this is - best just keep current zip file
+## ## create the zip file of data
+## unlink("eden_data.zip")
+## setwd("./_create_data")
+## zip("../eden_data.zip","./eden_data")
+## setwd("../")
 
 ## create the zip file of R scripts
 tmp <- list.files(".",pattern=".Rmd$")
@@ -19,7 +20,7 @@ unlink("./eden_data",recursive=TRUE)
 unzip("eden_data.zip")       
 
 ## run to build the site
-rmarkdown::clean_site(quiet=TRUE)
+rmarkdown::clean_site(quiet=TRUE,preview=FALSE)
 rmarkdown::render_site() #quiet=TRUE)
 
 ## create
